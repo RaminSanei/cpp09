30b1195e61a5ff7a41eeafb4ddbb477df0dd0108
Ali Sanei <ramin@alis-air-2.speedport.ip>
2025-02-21 13:56:16 +0100

    Add header file & Makefile according to subject.

---
+++ ex00/BitcoinExchange.hpp
@@ -0,0 +1,25 @@
+#ifndef BITCOIN_EXCHANGE_HPP
+#define BITCOIN_EXCHANGE_HPP
+#include <map>
+
+
+class BitcoinExchange
+{
+    private:
+    std::map<std::string, float> btcPrices;
+
+
+    void loadBitcoinDB(const std::string& dbPath);
+    std::string findClosestDate(const std::string& targetDate) const;
+    bool isValidValue(float value) const;
+
+public:
+    BitcoinExchange();
+    BitcoinExchange(const std::string& dbPath);
+    ~BitcoinExchange();
+    BitcoinExchange(const BitcoinExchange& other);
+    BitcoinExchange& operator=(const BitcoinExchange& other);
+
+    void processInputFile(const std::string& inputFilePath) const;
+};
+#endif

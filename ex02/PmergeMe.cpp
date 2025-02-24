d8227c16d80e30631f241ecae7e998f8597d71a6
Ali Sanei <ramin@alis-air-2.speedport.ip>
2025-02-24 11:17:54 +0100

    Implementing behavior of member functions.

--- ex02/PmergeMe.cpp
+++ ex02/PmergeMe.cpp
@@ -18,6 +18,52 @@ PmergeMe &PmergeMe::operator=(const PmergeMe &other)
 
 PmergeMe::~PmergeMe() {}
 
+template <typename Container>
+void PmergeMe::mergeInsertionSort(Container &container)
+{
+    if (container.size() <= 1)
+        return;
+
+    typename Container::iterator middle = container.begin() + container.size() / 2;
+    Container left(container.begin(), middle);
+    Container right(middle, container.end());
+
+    mergeInsertionSort(left);
+    mergeInsertionSort(right);
+
+    std::merge(left.begin(), left.end(), right.begin(), right.end(), container.begin());
+}
+
+template <typename Container>
+void PmergeMe::displaySequence(const std::string &message, const Container &container) const
+{
+    std::cout << message;
+    for (typename Container::const_iterator it = container.begin(); it != container.end(); ++it)
+    {
+        std::cout << *it << " ";
+    }
+    std::cout << std::endl;
+}
+
+template <typename Container>
+void PmergeMe::sortWithContainer(const Container &sequence, const std::string &containerName)
+{
+    Container container(sequence);
+    displaySequence("Before: ", container);
+
+    std::clock_t start = std::clock();
+    mergeInsertionSort(container);
+    double elapsedTime = getElapsedTime(start);
+
+    displaySequence("After: ", container);
+    std::cout << "Time to process a range of " << container.size() << " elements with " << containerName << ": "
+              << std::fixed << std::setprecision(5) << elapsedTime << " us" << std::endl;
+}
+
+double PmergeMe::getElapsedTime(const std::clock_t &start) const
+{
+    return (std::clock() - start) / (double)CLOCKS_PER_SEC * 1000000; 
+}
 
 void PmergeMe::validateInput(int argc, char *argv[])
 {
